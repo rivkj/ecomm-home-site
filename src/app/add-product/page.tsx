c922350f9bbbd6c4d956e45f6e0d2009429b45bd
rivkj <rivaskj07@gmail.com>
2024-05-15 18:35:23 -0400

    Added redirect after adding information to client side, styled button accordingly

--- src/app/add-product/page.tsx
+++ src/app/add-product/page.tsx
@@ -1,12 +1,35 @@
+import FormSubmitButton from "@/components/FormSubmitButton";
+import { prisma } from "@/lib/db/prisma";
+import { redirect } from "next/navigation";
+
 export const metadata = {
   title: "Add Product - Lumière Luxe & Co."
+}
+
+async function addProduct(formData: FormData) {
+  "use server";
+
+  const name = formData.get("name")?.toString();
+  const description = formData.get("description")?.toString();
+  const imageUrl = formData.get("imageUrl")?.toString();
+  const price = Number(formData.get("price") || 0);
+
+  if (!name || !description || !imageUrl || !price) {
+    throw Error("Missing required fields");
+  }
+
+  await prisma.product.create({
+    data: {name, description, imageUrl, price}
+  });
+
+  redirect("/");
 }
 
 export default function AddProductPage() {
   return (
     <div>
       <h1 className="mb-3 text-lg font-bold">Add Product</h1>
-      <form>
+      <form action={addProduct}>
         <input 
           required
           name="name"
@@ -32,7 +55,9 @@ export default function AddProductPage() {
           type="number"
           className="input-bordered input mb-3 w-full "
         />
-        <button className="btn btn-primary btn-block" type="submit">Add Product</button>
+        <FormSubmitButton className="btn-block" type="submit">
+          Add Product
+        </FormSubmitButton>
       </form>
     </div>
   );
