3d8f344627774fa323db5c10ea33756f86514635
rivkj <rivaskj07@gmail.com>
2024-05-21 18:37:35 -0400

    set up cart in db and page, loading state and cart values

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -4,21 +4,24 @@ import { Metadata } from "next";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 import { cache } from "react";
+import AddToCartButton from "./AddToCartButton";
+import { incrementProductQuantity } from "../actions";
 
 interface ProductPageProps {
   params: {
-    id: string,
-  }
+    id: string;
+  };
 }
 
 const getProduct = cache(async (id: string) => {
-  const product = await prisma.product.findUnique({where: {id}})
+  const product = await prisma.product.findUnique({where: { id } })
   if(!product) notFound();
   return product;
 })
 
 export async function generateMetadata(
-  {params: {id}} : ProductPageProps
+  {params: {id},
+} : ProductPageProps
 ): Promise<Metadata> {
   const product = await getProduct(id);
 
@@ -31,13 +34,14 @@ export async function generateMetadata(
   }
 };
 
-export default async function ProductPage(
-  {params: {id}} : ProductPageProps
+export default async function ProductPage({
+  params: { id },
+} : ProductPageProps
 ) {
   const product = await getProduct(id);
 
   return(
-    <div className="flex flex-col lg:flex-row gap-4 lg:items-center">
+    <div className="flex flex-col gap-4 lg:flex-row lg:items-center">
       <Image 
         src={product.imageUrl}
         alt={product.name}
@@ -51,6 +55,9 @@ export default async function ProductPage(
         <h1 className="text-5xl font-bold">{product.name}</h1>
         <PriceTag price={product.price} className="mt-4" />
         <p className="py-6">{product.description}</p>
+        <AddToCartButton 
+        productId={product.id} 
+        incrementProductQuantity={incrementProductQuantity} />
       </div>
     </div>
   );
