17ef79407e1db23e0ba49630a36d7f6f9a336d4f
rivkj <rivaskj07@gmail.com>
2024-05-20 20:29:59 -0400

    Added additional products to home page and edited styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,16 +20,22 @@ export default async function Home() {
           className="w-full max-w-sm rounded-lg shadow-2xl"
           priority
         />
-        <div>
-          <h1 className="text-5xl font-bold">{products[0].name}</h1>
-          <p className="py-6">{products[0].description}</p>
-          <Link
-            href={"/products/" + products[0].id}
-            className="btn-primary btn">
-              Check it out
-          </Link>
+          <div>
+            <h1 className="text-5xl font-bold">{products[0].name}</h1>
+            <p className="py-6">{products[0].description}</p>
+            <Link
+              href={"/products/" + products[0].id}
+              className="btn-primary btn">
+                Check it out
+            </Link>
+          </div>
         </div>
-        </div>
+      </div>
+
+      <div className="my-4 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+        {products.slice(1).map(product => (
+          <ProductCard product={product} key={product.id} />
+        ))}
       </div>
     </div>
   );
