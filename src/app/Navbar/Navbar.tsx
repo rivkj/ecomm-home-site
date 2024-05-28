8164d3f56f2ead65fa28df2cb737762a404585a8
rivkj <rivaskj07@gmail.com>
2024-05-28 12:42:07 -0400

    Added search menu, cart and dropdown menu

--- src/app/Navbar/Navbar.tsx
+++ src/app/Navbar/Navbar.tsx
@@ -1,16 +1,44 @@
 import Image from "next/image";
 import Link from "next/link";
 import logo from "@/assets/logo.png";
+import { redirect } from "next/navigation";
+import { getCart } from "@/lib/db/cart";
+import ShoppingCartButton from "./ShoppingCartButton";
 
-export default function Navbar() {
+async function searchProducts(formData: FormData) {
+  "use server";
+
+  const searchQuery = formData.get("searchQuery")?.toString();
+
+  if (searchQuery) {
+    redirect("/search?query=" + searchQuery);
+  }
+}
+
+export default async function Navbar() {
+  const cart = await getCart();
+
   return(
     <div className="bg-base-100">
       <div className="navbar max-w-7xl m-auto flex-col sm:flex-row gap-2">
         <div className="flex-1">
-          <Link href="/" className="btn btn-ghost text-xl">
-            <Image src={logo} height={120} width={120} alt="Lumière Luxe & Co. logo" />
+          <Link href="/" className="btn btn-ghost text-xl normal-case">
+            <Image src={logo} height={80} width={80} alt="Lumière Luxe & Co. logo" />
             Lumière Luxe & Co.
           </Link>
+        </div>
+        <div className="flex-none gap-2">
+          <form action={searchProducts}>
+            <div className="form-control">
+              <input 
+                name="searchQuery"
+                placeholder="Search"
+                className="input input-bordered w-full min-w-[100px]" 
+              />
+            </div>
+          </form>
+
+          <ShoppingCartButton cart={cart} />
         </div>
       </div>
     </div>
